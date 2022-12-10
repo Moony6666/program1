014e56fe50d5a99124e01bb63bed8d7bb7121cec
Moonlight <stepa.barabashkov07@gmail.com>
2022-12-10 18:56:32 +0300

    остаток и деление

---
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int first, second,third,four sum;
+
+	//Ввод данных 
+	cout << "Enter the divisible: ";
+	cin >> first;
+
+	cout << "Enter the divider: ";
+	cin >> second;
+
+	//Это вычислит частное и остаток
+	third = first / second;
+	four = first % second;
+	//Вывод данных с суммой
+	cout << "divisble = " << third << endl;
+	cout << "divider = " << four << endl;
+	return 0;
+}
