f4471c6c4700fcea020a0258551db0f166cb5e2a
Moonlight <stepa.barabashkov07@gmail.com>
2022-12-10 18:26:53 +0300

    Сумма двух целых чисел

---
+++ ConsoleApplication5/ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int first, second, sum;
+
+	//Ввод данных 
+	cout << "Enter two integers: ";
+	cin >> first >> second;
+
+	//Это вычислит сумму 
+	sum = first + second;
+	//Вывод данных с суммой
+	cout << first << "+" << second << "=" << sum << endl;
+	return 0;
+}
+ 
