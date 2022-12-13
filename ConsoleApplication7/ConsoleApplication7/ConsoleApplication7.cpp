5dde5f5e368956debdaa5bfeb2b13ba701006e92
Moonlight <stepa.barabashkov07@gmail.com>
2022-12-13 21:23:18 +0300

    Замена чисел

--- ConsoleApplication7/ConsoleApplication7/ConsoleApplication7.cpp
+++ ConsoleApplication7/ConsoleApplication7/ConsoleApplication7.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-	int first, second, third, four ;
+	int first, second;
 
 	//Ввод данных 
 	cout << "Enter the divisible: ";
@@ -12,9 +12,9 @@ int main()
 	cin >> second;
 
 	//Это вычислит частное и остаток
-	third = first / second;
-	four = first % second;
-	//Вывод данных с суммой
+	int third = first / second;
+	int four = first % second;
+	
 	cout << "divisble = " << third << endl;
 	cout << "divider = " << four << endl;
 	return 0;
---
+++ ConsoleApplication8/ConsoleApplication8/ConsoleApplication8.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a , b , temp;
+
+	cout << "Enter two numbers: " << endl;
+	cin >> a >> b;
+
+	cout << "Before replacement:" << endl;
+	cout << "a = " << a << ", b = " << b << endl;
+
+	temp = a;
+	a = b;
+	b = temp;
+	
+	cout << "After replacement:" << endl;
+	cout << "a = " << a << ", b = " << b << endl;
+
+	return 0;
+
+}
+
